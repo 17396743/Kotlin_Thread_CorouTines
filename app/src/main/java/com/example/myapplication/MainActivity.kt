ffd978d75ee926f38584b950289c994c4c7a1960
17396743 <1481583730@qq.com>
2021-06-17 17:18:27 +0800

    2021年6月17日上传

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,27 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.e(TAG, "主线程id：${mainLooper.thread.id}")
-        //runBlocking启动的协程任务会阻断当前线程，
-        // 直到该协程执行结束。
-        // 当协程执行结束之后，
-        // 页面才会被显示出来。
+        //1.runBlocking:T
         RunBlocking()
-        //launch不会阻断主线程。
+        //2.launch:Job
         Launch()
-        //suspend函数会将整个协程挂起，
-        // 而不仅仅是这个suspend函数，
-        // 也就是说一个协程中有多个挂起函数时，
-        // 它们是顺序执行的。
+        //3.协程体
         SusPendData()
-        //async跟launch的用法基本一样，
-        // 区别在于：async的返回值是Deferred，
-        // 将最后一个封装成了该对象。
-        // async可以支持并发 ，
-        // 此时一般都跟await一起使用
+        //4.async
         AsyncData()
+
         Log.e(TAG, "协程执行结束")
     }
 
+    /**
+     *  ==============================================
+     *  async跟launch的用法基本一样，
+     *  区别在于：async的返回值是Deferred，
+     *  将最后一个封装成了该对象。
+     *  async可以支持并发 ，
+     *  此时一般都跟await一起使用
+     *  ==============================================
+     */
     private fun AsyncData() {
         GlobalScope.launch {
             val result1 = GlobalScope.async {
@@ -56,7 +56,20 @@ class MainActivity : AppCompatActivity() {
         return 2
     }
 
-
+    /**
+     *  ==============================================
+     *  协程体是一个用suspend关键字修饰的一个无参，
+     *  无返回值的函数类型。
+     *  被suspend修饰的函数称为挂起函数,
+     *  与之对应的是关键字resume（恢复），
+     *  注意：挂起函数只能在协程中和其他挂起函数中调用，不能在其他地方使用。
+     *
+     *  suspend函数会将整个协程挂起，
+     *  而不仅仅是这个suspend函数，
+     *  也就是说一个协程中有多个挂起函数时，
+     *  它们是顺序执行的。
+     *  ==============================================
+     */
     private fun SusPendData() {
         GlobalScope.launch {
             val token = getToken()
@@ -83,6 +96,11 @@ class MainActivity : AppCompatActivity() {
         return "$token - userInfo"
     }
 
+    /**
+     *  ==============================================
+     *    launch不会阻断主线程。
+     *  ==============================================
+     */
     private fun Launch() {
         Log.e(TAG, "主线程id：${mainLooper.thread.id}")
         val job = GlobalScope.launch {
@@ -92,6 +110,14 @@ class MainActivity : AppCompatActivity() {
         Log.e(TAG, "主线程执行结束")
     }
 
+    /**
+     *  ==============================================
+     *  runBlocking启动的协程任务会阻断当前线程，
+     *  直到该协程执行结束。
+     *  当协程执行结束之后，
+     *  页面才会被显示出来。
+     *  ==============================================
+     */
     private fun RunBlocking() = runBlocking {
         //运行次数为8次
         repeat(8) {
@@ -100,8 +126,11 @@ class MainActivity : AppCompatActivity() {
             delay(1000)
         }
     }
-
-    //相关资料：https://www.jianshu.com/p/6e6835573a9c
+    /**
+     * ==============================================
+     *   相关资料：https://www.jianshu.com/p/6e6835573a9c
+     * ==============================================
+     */
 
 
 }
